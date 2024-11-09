030686de8ca63e8b5cde77bc90c06c4bbb3d7760
Zhendos1337 <zhendosnotdead@gmail.com>
2024-11-09 06:15:49 +0300

    2

---
+++ run.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+from app.main_menu import router
+from app.quests import router as r1
+from app.squads_about import router as r2
+from app.admins import router as r3
+from app.special import router as r4
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_routers(router, r1, r2, r3, r4)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except:
+            print('Exit')
+
+'''
+
+///сделать еще одно сообщение для ограничение задания///await bot.send_message(chat_id=-1002250236179, text='---------------')
+
+'''
+
